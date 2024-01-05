d3e5e501ec9c2574f3c40783b1fb6de9a68de19e
Tejas Pawar <140417793+Tejas0041@users.noreply.github.com>
2024-01-05 13:04:03 +0530

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -60,7 +60,7 @@ module.exports.isReviewAuthor= async(req, res, next)=>{
 }
 
 module.exports.isAdmin= async(req, res, next)=>{
-    if(!('65869c99a5db86bc05d7b34c'===(req.user._id)))
+    if(!('tejas'===(req.user.username)))
     {
         req.flash('error', 'You not authorized for that');
         return res.redirect(`/campground/map`);
